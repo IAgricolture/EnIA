0b5de273fbca09ec33139d22c23296e2cb30cbd0
benedettoscala <scala.benedetto@outlook.it>
2023-01-02 19:53:34 +0100

    Prova: primo commit con connessione al database

---
+++ src/logic/model/dbConnection.py
@@ -0,0 +1,9 @@
+import pymongo
+from pymongo import MongoClient
+
+#Connessione al provider mondoDbAltas
+client = MongoClient("mongodb+srv://enia:vp7CMyN7V5Rl8FZR@cluster0.o0m35vt.mongodb.net/?retryWrites=true&w=majority")
+
+#Assegnazione alla variabile db il database enia
+db = client.enia
+
