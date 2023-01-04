ba223d648ffed5fd446bacec7427da7dfbb0a92a
benedettoscala <scala.benedetto@outlook.it>
2023-01-04 23:24:52 +0100

    Added: Dao and entities Added DatiInquinamento, DatiInquinamentoDAO, Updated DatiMeteo and DatiMeteoDAO

--- src/logic/model/DatiMeteo.py
+++ src/logic/model/DatiMeteo.py
@@ -1,16 +1,14 @@
-
 import datetime
 
-
 class DatiMeteo():
-    def __init__(self, nome:str, data_rilevazione:datetime, estremo : bool, aqi : int, pm10 : int, pm25 :int, o3 : int, no2 : int, terreno: str, id = 0):
+    def __init__(self, nome : str, data_rilevazione : datetime, estremo : bool, pressione : int, umidità : int, temperatura : int, vento_direzione : int, vento_intensità : float,terreno : str, id = 0):
         self.nome = nome
         self.data_rilevazione = data_rilevazione
         self.estremo = estremo
-        self.aqi = aqi
-        self.pm10 = pm10
-        self.pm25 = pm25
-        self.o3 = o3
-        self.no2 = no2
+        self.pressione = pressione
+        self.umidità = umidità
+        self.temperatura = temperatura
+        self.vento_direzione = vento_direzione
+        self.vento_intensità = vento_intensità
         self.terreno = terreno
         self.id = id
