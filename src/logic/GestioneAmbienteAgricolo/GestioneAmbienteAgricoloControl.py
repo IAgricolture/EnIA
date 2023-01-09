d89b3822562cfe051d4c3271dcb866dd284fa96f
FraPCA <francesco3b@gmail.com>
2023-01-09 16:27:09 +0100

    fix: Fixed route handling for Flask
    Fixed bug where Flask would not handle routes from localhost

--- src/logic/GestioneAmbienteAgricolo/GestioneAmbienteAgricoloControl.py
+++ src/logic/GestioneAmbienteAgricolo/GestioneAmbienteAgricoloControl.py
@@ -7,29 +7,33 @@ from src.logic.model.TerrenoDAO import TerrenoDAO
 
 class AmbienteAgricoloControl():
 
-    @app.route("/aggiuntaterreno", methods=["POST"])
+    @app.route("/aggiuntaTerreno", methods=["POST", "GET"])
     def aggiungiTerreno():
-        richiesta = request.get_json()
-        id = None
-        nome = richiesta.get("nome")
-        coltura = richiesta.get("coltura")
-        posizione = richiesta.get("posizione")
-        preferito = richiesta.get("preferito")
-        priorita = richiesta.get("priorita")
+        if(request.method != "POST"):
+            return render_template("aggiuntaterreno.html")
+        elif request.method == "POST":
+            
+            richiesta = request.get_json()
+            id = None
+            nome = richiesta.get("nome")
+            coltura = richiesta.get("coltura")
+            posizione = richiesta.get("posizione")
+            preferito = richiesta.get("preferito")
+            priorita = richiesta.get("priorita")
 
-        risposta = {
-            "TerrenoAggiunto" : False
-        }
+            risposta = {
+                "TerrenoAggiunto" : False
+            }
 
-        #MeMO: per salvarlo ne DB
-        NewTerreno = Terreno(id,nome,coltura,posizione,preferito,priorita)
-        TerrenoDAO.InserisciTerreno(NewTerreno)
+            #MeMO: per salvarlo ne DB
+            NewTerreno = Terreno(id,nome,coltura,posizione,preferito,priorita)
+            TerrenoDAO.InserisciTerreno(NewTerreno)
 
-        risposta["TerrenoAggiunto"] = True
-        #TODO implememtare i controlli per l'aggiunta
+            risposta["TerrenoAggiunto"] = True
+            #TODO implememtare i controlli per l'aggiunta
 
-        #Invio della risposta al server in formato json
-        return jsonify(risposta)
+            #Invio della risposta al server in formato json
+            return jsonify(risposta)
 
 
     @app.route("/modificaTerreno", methods=["POST", "GET"])
@@ -54,24 +58,27 @@ class AmbienteAgricoloControl():
 
 
 
-    @app.route("/eliminaTerreno", methods = ["GET"])
+    @app.route("/eliminaTerreno", methods = ["POST", "GET"])
     def elimina():
         """ 
         #TODO Scrivere documenzione
         """
-        idTerreno : str = request.args.get("idTerreno") #Se non str, sicuramente fallisce, trovaTerreno si aspetta str
-        print("idTerreno inserito: " + idTerreno)
-        if len(idTerreno) != 24:    #Controllo che sia un id MongoDB valido
-            print("Errore: Id inserito non valido.")
-            
-        terreno = TerrenoDAO.TrovaTerreno(idTerreno)
-        if terreno is None: #Non l'ha trovato
-            print("Nessun Terreno trovato con questo id")
-            return jsonify({"trovato" : "false"})
-        else:  
-            TerrenoDAO.RimuoviTerreno(terreno)
-            print("Terreno Eliminato ")
-            return jsonify({ "trovato": "true"})
+        if(request.method != "POST"):
+            return render_template("eliminaterreno.html")
+        elif request.method == "POST":
+            idTerreno : str = request.args.get("idTerreno") #Se non str, sicuramente fallisce, trovaTerreno si aspetta str
+            print("idTerreno inserito: " + idTerreno)
+            if len(idTerreno) != 24:    #Controllo che sia un id MongoDB valido
+                print("Errore: Id inserito non valido.")
+                
+            terreno = TerrenoDAO.TrovaTerreno(idTerreno)
+            if terreno is None: #Non l'ha trovato
+                print("Nessun Terreno trovato con questo id")
+                return jsonify({"trovato" : "false"})
+            else:  
+                TerrenoDAO.RimuoviTerreno(terreno)
+                print("Terreno Eliminato ")
+                return jsonify({ "trovato": "true"})
         
     @app.route("/dettagliterreno", methods = ["POST", "GET"])
     def dettagli():
