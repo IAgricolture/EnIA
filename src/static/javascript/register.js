2537c7608f735e5948622f74153dae356c53e4d5
FraPCA <francesco3b@gmail.com>
2023-01-22 20:00:16 +0100

    chore: Fix merge issue
    Fixed register.js directly pushed

--- src/static/javascript/register.js
+++ src/static/javascript/register.js
@@ -1,5 +1,4 @@
-function register(){
-        
+function register() {
     var nome = document.getElementById("nome").value
     var cognome = document.getElementById("cognome").value
     var email = document.getElementById("email").value
@@ -8,22 +7,83 @@ function register(){
     var codiceDiAccesso = document.getElementById("codiceDiAccesso").value
     var indirizzo = document.getElementById("indirizzo").value
     var dataNascita = document.getElementById("dataNascita").value
-    console.log(dataNascita)
 
-    var labelEmail = document.getElementById("labelEmail")
-    var formPassword = document.getElementById("password")
-    var labelPassword = document.getElementById("labelPassword")
-    var labelCodice = document.getElementById("labelCodice")
-    var formRegistrazione = document.getElementById("formRegistrazione")
-    
-    if( password != confermaPassword){
-      
-      labelPassword.innerHTML = "Le due password non corrispondono"
-      formPassword.classList.add("has-error")
-      formPassword.classList.add("has-feedback")
+    var nomeerr = document.getElementById("nomeerr")
+    var cognomeerr = document.getElementById("cognomeerr")
+    var emailerr = document.getElementById("emailerr")
+    var passworderr = document.getElementById("passerr")
+    var confermaerr = document.getElementById("conferr")
+    var cod = document.getElementById("cod")
+    var indirizzoerr = document.getElementById("indirizzoerr")
+    var dataNascitaerr = document.getElementById("dataerr")
+
+    const nomereg = /^[a-z ]{2,30}$/i
+    const mailreg = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/i
+    const passreg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&_])[A-Za-z\d$@$!%*?&_]{8,20}$/
+    const indireg = /^[A-Za-zÀ-ù0-9 ,‘-]+$/
+
+    var register = true
+
+    nomeerr.innerHTML = ""
+    cognomeerr.innerHTML = ""
+    emailerr.innerHTML = ""
+    passworderr.innerHTML = ""
+    confermaerr.innerHTML = ""
+    cod.innerHTML = ""
+    indirizzoerr.innerHTML = ""
+    dataNascitaerr.innerHTML = ""
+
+    if (!nomereg.test(nome)) {
+      if (nome == "")
+        nomeerr.innerHTML = "Campo Richiesto"
+      else
+        nomeerr.innerHTML = "Formato non valido"
+      register = false
     }
-    else{
-      
+
+    if (!nomereg.test(cognome)) {
+      if (cognome == "")
+        cognomeerr.innerHTML = "Campo Richiesto"
+      else
+        cognomeerr.innerHTML = "Formato non valido"
+      register = false
+    }
+
+    if (!mailreg.test(email)) {
+      if (email == "")
+        emailerr.innerHTML = "Campo Richiesto"
+      else
+        emailerr.innerHTML = "Formato non valido"
+      register = false
+    }
+
+    if (!passreg.test(password)) {
+      if (password == "")
+        passworderr.innerHTML = "Campo Richiesto"
+      else
+        passworderr.innerHTML = "Formato non valido"
+      register = false
+    }
+
+    if (password != confermaPassword) {
+      confermaerr.innerHTML = "Le due password non corrispondono"
+      register = false
+    }
+
+    if (!indireg.test(indirizzo)) {
+      if (indirizzo == "")
+        indirizzoerr.innerHTML = "Campo Richiesto"
+      else
+        indirizzoerr.innerHTML = "Formato non valido"
+      register = false
+    }
+
+    if (dataNascita == "") {
+      dataNascitaerr.innerHTML = "Campo Richiesto"
+      register = false
+    }
+
+    if (register) {
       var data = new FormData()
       data.append("nome", nome)
       data.append("cognome", cognome)
@@ -33,24 +93,20 @@ function register(){
       data.append("indirizzo", indirizzo)
       data.append("dataNascita", dataNascita)
 
-      let response = fetch("register",
-      {
-        "method": "POST",
-        "headers": {},
-        "body":data
-      }).then(res=>res.json())
-      .then(data=>{
+      let response = fetch("register", {
+          "method": "POST",
+          "headers": {},
+          "body": data
+        }).then(res => res.json())
+        .then(data => {
           console.log(data)
-          if(data["emailUsata"]){
-            labelEmail.innerHTML += ":email già usata"
-          } else if (data["codiceNonValido"]){
-            labelCodice.innerHTML += ":codice non valido o già usato"
-          } else if (data["utenteRegistrato"]){
-            labelCodice.innerHTML += ":utenteNonRegistrato"
-          } else{
-            labelCodice.innerHTML += "utenteRegistrato"
+          if (data["emailUsata"]) {
+            emailerr.innerHTML = "email già usata"
+          } if (data["codiceNonValido"]) {
+            cod.innerHTML = "codice non valido o già usato"
+          }if(data["utenteRegistrato"]){
+              window.location.href ="/login"
           }
-      })
+        })
     }
-    
-}
+  }
