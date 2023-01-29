7eb3d2184d44874d626d45088bb89bc64e1183b3
benedettoscala <scala.benedetto@outlook.it>
2023-01-29 18:42:20 +0100

    Added IAdapter to the adapter folder

--- src/logic/DecisionIntelligence/DecisionIntelligenceService.py
+++ src/logic/DecisionIntelligence/DecisionIntelligenceService.py
@@ -1,6 +1,7 @@
-from src.logic.DecisionIntelligence.IAdapter import IAdapter
+from src.Adapters.IAdapter import IAdapter
 
 
 class DecisionIntelligenceService:  
     def getPredizioneLivelliIrrigazione(lon : float, lat : float, crop : str, stage : str):
-        return IAdapter.getAiPrediction(lat, lon, crop, stage)
+        adapter = IAdapter(lat, lon, crop, stage)
+        return adapter.getAiPrediction()
