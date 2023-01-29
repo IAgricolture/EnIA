724d214ab7ea060369f1c2c30e5104c3a58cc5d9
benedettoscala <scala.benedetto@outlook.it>
2023-01-29 18:32:57 +0100

    Added sensesquareadapter

--- src/logic/AmbienteAgricolo/AmbienteAgricoloService.py
+++ src/logic/AmbienteAgricolo/AmbienteAgricoloService.py
@@ -1,4 +1,5 @@
 from src.Adapters.NominatimAdapter import NominatimAdapter
+from src.Adapters.SenseSquareAdapter import SenseSquareAdapter
 from src.logic.DecisionIntelligence.DecisionIntelligenceService import DecisionIntelligenceService
 from src.logic.GestioneEventi.GestioneEventiService import GestioneEventiService
 from src.logic.Storage.ImpiantoDiIrrigazioneDAO import ImpiantoDiIrrigazioneDAO
@@ -113,54 +114,14 @@ class AmbienteAgricoloService():
             return datiapi  #JSON
         
     def cercaInquinamento(provincia:str, regione:str, nazione:str, comune:str):
-        currentdate = str(datetime.now()).split(" ")
-        date = currentdate[0]
-        precisehour = currentdate[1].split(":")
-        time = precisehour[0] + ":" + precisehour[1]
-        url = "https://square.sensesquare.eu:5001/placeView"
-        body = {
-            "apikey": "3BK3D0LWE8DQ", #Codice API, NON MODIFICARE
-            "tempo": "giorno",
-            "date": date,
-            "time": time,
-            "nazione": nazione,
-            "regione": regione,
-            "provincia": provincia,
-            "comune": comune,
-            "zoom": "3", #0 per tutte le nazioni, 1 per tutte le regioni, 2 per tutte le province, 3 per tutti i comuni, 4 NON VA
-            "predictions": "true", #NON TOCCARE
-            "fonti":"[]" #NON TOCCARE
-            }
-        datiapi = requests.post(url=url, data = body).json()
-        print(datiapi)
+        adapter = SenseSquareAdapter(nazione, regione, provincia, comune)
+        datiapi = adapter.get_data_for_today()
         return datiapi
     
     def cercaStoricoInquinamento(dataInizio:str, dataFine:str, comune:str, regione:str, nazione:str, provincia:str, formato:str):
-        url = "https://square.sensesquare.eu:5001/download"
-        body = {
-            "apikey": "3BK3D0LWE8DQ",
-            "req_type": "daily",
-            "zoom": "3",
-            "start_date": dataInizio,    #Formato deve essere YYYY-MM-DD
-            "start_hour": "0",
-            "end_date": dataFine,
-            "end_hour": "0",
-            "nazione": nazione,
-            "regione": regione,
-            "provincia": provincia,
-            "comune": comune,
-            "format": formato
-        }
-        datiapi = requests.post(url=url, data = body).text
-        print(datiapi)
-        if(formato == "json"):  ##Multipli oggetti JSON, ne faccio un parse decente in un array di oggetti json.
-            arrayDati = datiapi.split("\n") #Ne ottengo un array
-            print(arrayDati)
-            print(len(arrayDati))
-            arrayDati.pop(len(arrayDati) - 1) #Rimuovo un elemento vuoto creato con lo split, all'ultimo posto
-            return arrayDati
-        else:
-            return datiapi  #Qualsiasi altro formato, è buono così com'è.
+        adapter = SenseSquareAdapter(nazione, regione, provincia, comune, start_date = dataInizio, end_date = dataFine, formato = formato)
+        datiapi = adapter.get_data_time_interval()
+        return datiapi
 
     def aggiungiIrrigatore(id_terreno: str, nome_irrigatore: str, posizione_irrigatore: str) -> str:
         impianto = ImpiantoDiIrrigazione("", nome_irrigatore, "irrigatore", "", posizione_irrigatore, False)
