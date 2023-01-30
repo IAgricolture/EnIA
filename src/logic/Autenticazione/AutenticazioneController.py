deb6af1cea45870c22d99cd7faebc4d82017ad2d
OddSnike <francesco.fattorusso@icloud.com>
2023-01-30 15:24:52 +0100

    Chore: Added docstring to Autentiocazione

--- src/logic/Autenticazione/AutenticazioneController.py
+++ src/logic/Autenticazione/AutenticazioneController.py
@@ -5,40 +5,60 @@ from src.logic.Autenticazione.AutenticazioneService import AutenticazioneService
 from flask import request, render_template, redirect, url_for
 from src import app
 
+
 class AutenticazioneController():
-    #This method is mandatory to use the flask_login module
+    '''
+    Classe Controller di Autenticazione
+
+    ...
+
+    Attributi
+    ----------
+    None
+
+    Metodi
+    -------
+    load_user(user_id):
+        Carica un utente dalla sessione utilizzando il suo id univoco
+    login():
+        Controlla le credenziali inserite dall'utente, se c'è una corrispondenza sul database il sistema
+        cambia il suo stato da anonimo ad utente loggato
+    logout():
+        Effettua il logout di un utente
+
+    '''
+    # This method is mandatory to use the flask_login module
     @login_manager.user_loader
     def load_user(user_id):
-        """
-            This method tells flask how to load a user from its session using an unique id
-            :return: Utente
-        """
+        '''Carica un utente dalla sessione utilizzando il suo id univoco'''
         return AutenticazioneService.trovaUtenteById(user_id)
 
-    @app.route("/login", methods = ["GET", "POST"])
+    @app.route("/login", methods=["GET", "POST"])
     def login():
-        """
-        Reads the users credentials from a https request if they match with one entry
-        on the database, the system change his state from anonymous to logged user
-        :return: redirect to index page
-        """
-        if request.method == "POST" :
+        '''Controlla le credenziali inserite dall'utente, se c'è una corrispondenza
+        sul database il sistema cambia il suo stato da anonimo ad utente loggato'''
+        if request.method == "POST":
             email = request.form.get("email")
             password = request.form.get("password")
             urlLastPage = request.form.get("next")
             redirectUrl = "visualizzaTerreni"
             successo = AutenticazioneService.login(email, password)
             if successo:
-                #Se ha provato ad accedere ad una pagina prima di arrivare al login automaticamente:
-                if(urlLastPage):
+                # Se ha provato ad accedere ad una pagina prima di arrivare al
+                # login automaticamente:
+                if (urlLastPage):
                     redirectUrl = urlLastPage
                 return redirect(redirectUrl)
             else:
-                return render_template("login.html")        
+                return render_template("login.html")
         else:
             return render_template("login.html")
 
     @app.route("/logout")
     def logout():
+        '''Effettua il logout di un utente'''
         AutenticazioneService.logout()
-        return redirect(url_for("login")) #Uso url_for per generare un url valido per login: render_template copia nell'url logout, che per qualche motivo crea problemi con il redirect.
+        # Uso url_for per generare un url valido per login: render_template
+        # copia nell'url logout, che per qualche motivo crea problemi con il
+        # redirect.
+        return redirect(url_for("login"))
--- src/logic/Autenticazione/AutenticazioneService.py
+++ src/logic/Autenticazione/AutenticazioneService.py
@@ -7,34 +7,98 @@ from datetime import timedelta
 
 from flask_login import login_user, logout_user
 
+
 class AutenticazioneService():
-    
-    def login(email:str, password:str) -> bool:
+    '''
+    Classe Service di Autenticazione
+
+    ...
+
+    Attributi
+    ----------
+    None
+
+    Metodi
+    -------
+
+    '''
+
+    def login(email: str, password: str) -> bool:
+        '''
+        Controlla se le credenziali di accesso sono presentio sul database
+
+        Parametri
+        ----------
+        email : str
+            E-mail
+        password : str
+            password
+
+        Returns
+        -------
+        successo : bool
+            Esito del login
+        '''
         successo = False
         hashed_password = hashlib.sha512(password.encode()).hexdigest()
-        login_attempt : Utente = AutenticazioneDAO.trovaUtenteByEmail(email)
-        
+        login_attempt: Utente = AutenticazioneDAO.trovaUtenteByEmail(email)
+
         if not login_attempt:
             print("Utente non registrato")
             successo = False
-            
+
         if login_attempt.password == hashed_password:
             login_user(login_attempt, duration=timedelta(days=365), force=True)
             successo = True
-                
+
         else:
             print("password errata")
             successo = False
-        
+
         return successo
-    
-    def trovaUtenteById(id:str)-> Utente:
+
+    def trovaUtenteById(id: str) -> Utente:
+        '''
+        Cerca un utente sul DB attraverso il suo id
+
+        Parametri
+        ----------
+        id : str
+
+        Returns
+        -------
+        AutenticazioneDAO.trovaUtente(id): Utente
+            Utente trovato sul DB
+        '''
         return AutenticazioneDAO.trovaUtente(id)
-    
+
     def logout():
+        '''
+        Effettua il logout
+
+        Parametri
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         logout_user()
         print("Logout utente")
-    
+
     def getDatoreFromDipendente(idDipendente: str) -> Utente:
+        '''
+        Recupera il datore del dipendente
+
+        Parametri
+        ----------
+        idDipendente : str
+            Id del dipendente
+
+        Returns
+        -------
+        AutenticazioneDAO.getDatore(idDipendente) : Utente
+            Il Datore del Utente
+        '''
         return AutenticazioneDAO.getDatore(idDipendente)
-        
