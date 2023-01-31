612ce467833355af651d41c968cd0a925698a7c4
Paneenutella0654 <104584785+Paneenutella0654@users.noreply.github.com>
2023-01-31 18:31:16 +0100

    Chore: Added docstring to DecisioniIntelligenceSer

--- src/logic/DecisionIntelligence/DecisionIntelligenceService.py
+++ src/logic/DecisionIntelligence/DecisionIntelligenceService.py
@@ -1,7 +1,26 @@
 from src.logic.Adapters.IAdapter import IAdapter
 
 
-class DecisionIntelligenceService:  
+class DecisionIntelligenceService():  
+    '''
+    Classe Service di Decision Inteligence
+
+    ...
+
+    Attributi
+    ----------
+    None
+
+    Metodi
+    getPredizioneLivelliIrrigazione(lon : float, lat : float, crop : str, stage : str)
+        Dati i valori di latitudine, longitudine la tipologia di crops ed lo stato di crescita
+        restiuisce le previsioni meteo 
+
+    '''
     def getPredizioneLivelliIrrigazione(lon : float, lat : float, crop : str, stage : str):
+        '''
+        Dati i valori di latitudine, longitudine la tipologia di crops ed lo stato di crescita
+        restiuisce le previsioni meteo 
+        '''
         adapter = IAdapter(lat, lon, crop, stage)
         return adapter.getAiPrediction()
