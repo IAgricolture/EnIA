a18a4d1016bee02e0f7e8bf83fc530cd6a601f39
Paneenutella0654 <104584785+Paneenutella0654@users.noreply.github.com>
2023-01-31 18:56:12 +0100

    Chore: Added docstring to GestioneSheduleService

--- src/logic/GestioneSchedule/GestioneScheduleService.py
+++ src/logic/GestioneSchedule/GestioneScheduleService.py
@@ -6,8 +6,37 @@ from src.logic.model.Evento import Evento
 
 
 class GestioneScheduleService:
-    
+    '''
+    Classe Service di Autenticazione
+
+    ...
+
+    Attributi
+    ----------
+    None
+
+    Metodi
+    trovaScheduleTerreno(idTerreno : str):
+        Recupera i dati riguardanti lo schedule per l'irrigazione per il terreno passato in input
+    modificaLivelloSchedule(id_terreno: str, data: str, modalita: str):
+
+    usaSchedulingConsigliato(id_terreno: str, lat: float, lon: float, stage: str, coltura: str):
+
+    '''
     def trovaScheduleTerreno(idTerreno : str):
+        '''
+        Recupera i dati riguardanti lo schedule per l'irrigazione per il terreno passato in input
+
+        Parametri
+        ----------
+        idTerreno : str
+            id del Terreno
+
+        Returns
+        -------
+        dict : dict
+            Schedule salvato nel DataBase
+        '''
         schedule_settimanale = ScheduleDAO.getWeeklySchedule(idTerreno)
         dict = {}
         #push in dict the schedule
@@ -16,10 +45,48 @@ class GestioneScheduleService:
         return dict
     
     def modificaLivelloSchedule(id_terreno: str, data: str, modalita: str):
+        '''
+        Aggiorna le informazioni sul database in base alle informaizoni passate in input 
+
+        Parametri
+        ----------
+        id_terreno: str 
+            id del Terrreno
+        data: str
+            Variabile riferita alla data in vui esegluimo la modifica
+        modalita: str
+            Variabile che indica la modalita di irrigazione
+
+        Returns
+        -------
+        True : bool
+            Esito della modifica
+        '''
         ScheduleDAO.modificaLivelloSchedule(id_terreno, data, modalita)
         return True
     
     def usaSchedulingConsigliato(id_terreno: str, lat: float, lon: float, stage: str, coltura: str):
+        '''
+        Setta lo Scheduling in base alle inforrmazione risultanti dalle previsioni su quel determinato terreno 
+
+        Parametri
+        ----------
+        id_terreno: str
+            id del Terreno
+        lat: float
+            Latitudine del Terreno
+        lon: float
+            Longitudine del Terreno
+        stage: str
+            Livello di crescita 
+        coltura: str
+            Tipologia di coltivazione in quel terreno
+
+        Returns
+        -------
+        True : bool
+            Esito positivo 
+        '''
         dict = DecisionIntelligenceService.getPredizioneLivelliIrrigazione(lon, lat, coltura, stage)
         #per ogni data nel dizionario
         for data in dict:
