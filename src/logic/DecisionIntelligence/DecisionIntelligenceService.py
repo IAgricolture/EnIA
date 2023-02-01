83d7e1aa52ef370f0cb99970fb2088a81d4b7fa6
marialombardi861 <marialombardi828@gmail.com>
2023-02-01 09:55:46 +0100

    Chore: standard pep8 for Decision Intelligence

--- src/logic/DecisionIntelligence/DecisionIntelligenceService.py
+++ src/logic/DecisionIntelligence/DecisionIntelligenceService.py
@@ -1,7 +1,7 @@
 from src.logic.Adapters.IAdapter import IAdapter
 
 
-class DecisionIntelligenceService():  
+class DecisionIntelligenceService():
     '''
     Classe Service di Decision Inteligence
 
@@ -14,13 +14,14 @@ class DecisionIntelligenceService():
     Metodi
     getPredizioneLivelliIrrigazione(lon : float, lat : float, crop : str, stage : str)
         Dati i valori di latitudine, longitudine la tipologia di crops ed lo stato di crescita
-        restiuisce le previsioni meteo 
+        restiuisce le previsioni meteo
 
     '''
-    def getPredizioneLivelliIrrigazione(lon : float, lat : float, crop : str, stage : str):
+    def getPredizioneLivelliIrrigazione(
+            lon: float, lat: float, crop: str, stage: str):
         '''
         Dati i valori di latitudine, longitudine la tipologia di crops ed lo stato di crescita
-        restiuisce le previsioni meteo 
+        restiuisce le previsioni meteo
         '''
         adapter = IAdapter(lat, lon, crop, stage)
         return adapter.getAiPrediction()
