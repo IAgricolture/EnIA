2b8abd2b1fb2949ba9417d896399a413f0299f37
Paneenutella0654 <gerardofrino01@gmail.com>
2023-02-01 19:59:43 +0100

    CHORE : Adding docstring for ImpiantoDiIrriga..DAO

--- src/logic/Storage/ImpiantoDiIrrigazioneDAO.py
+++ src/logic/Storage/ImpiantoDiIrrigazioneDAO.py
@@ -6,8 +6,49 @@ from bson.objectid import ObjectId
 
 
 class ImpiantoDiIrrigazioneDAO():
+    '''
+    Classe DAO di Impianto di Irrigazione 
 
+    ...
+
+    Attributi
+    ----------
+    None
+
+    Metodi
+    -------
+    findImpiantiByTerreno(idTerreno: str):
+        Questo metodo trova tutti gli impiati di irrigazioni associati a id del terreno 
+    findImpiantoById(id: str):
+        Questo metodo trova un Impianto di irrigazione sul database, usando il suo ObjectId
+    creaImpianto(impianto: ImpiantoDiIrrigazione, idTerreno: str)        
+        Questo metodo instanzia un impianto di irrigazione sul database
+    modificaImpianto(impianto: ImpiantoDiIrrigazione):
+        Questo metodo permette la modifica di un impianto di irrigazione sul database
+    attivaImpianto(id: str):
+        Questo metodo attiva un impianto di irrigazione sul database tramite il suo id 
+    disattivaImpianto(id: str):
+        Questo metodo disattiva un impianto di irrigazione sul database tramite il suo id 
+    eliminaImpianto(id: str):
+        Questo metodo elimina un impianto di irrigazione sul database tramite il suo id 
+
+    '''
+    
     def findImpiantiByTerreno(idTerreno: str):
+        '''
+        Questo metodo trova tutti gli impiati di irrigazioni associati a id del terreno 
+
+        Parametri
+        ----------
+        idTerreno: str
+            id del Terreno
+
+        Returns
+        -------
+        impiantiTrovato : ImpiantoDiIrrigazione
+            Restituisce una lista contenete gli impianti trovati 
+        '''
+        
         # trova impianti sul database con l'id del terreno
         impiantiTrovati = impianti.find({"terreno": ObjectId(idTerreno)})
         impiantiTrovati = list(impiantiTrovati)
@@ -20,10 +61,19 @@ class ImpiantoDiIrrigazioneDAO():
         return list(impiantiTrovati)
 
     def findImpiantoById(id: str) -> ImpiantoDiIrrigazione:
-        """
-            Questo metodo trova un Impianto di irrigazione sul database, usando il suo ObjectId
-            :return: ImpiantoDiIrrigazione
-        """
+        '''
+        Questo metodo trova un Impianto di irrigazione sul database, usando il suo ObjectId
+
+        Parametri
+        ----------
+        id: str
+            id del Impianto
+
+        Returns
+        -------
+        impiantoTrovato : ImpiantoDiIrrigazione
+            Restituisce l'occorenza trovata con quell id 
+        '''
         trovato = impianti.find_one({"_id": ObjectId(id)})
 
         id = str(trovato.get("_id"))
@@ -39,9 +89,21 @@ class ImpiantoDiIrrigazioneDAO():
         return impiantoTrovato
 
     def creaImpianto(impianto: ImpiantoDiIrrigazione, idTerreno: str) -> str:
-        """
-            Questo metodo instanzia un impianto di irrigazione sul database
-        """
+        '''
+        Questo metodo instanzia un impianto di irrigazione sul database
+
+        Parametri
+        ----------
+        idTerreno: str
+            id del Terreno
+        Impianto: ImpiantoDiIrrigazione
+            istanza da craeare nel DataBase
+
+        Returns
+        -------
+        result : str
+            Restituisce l'id del impianto appena craeato 
+        '''
 
         result = impianti.insert_one({
             "nome": impianto.nome,
@@ -55,9 +117,17 @@ class ImpiantoDiIrrigazioneDAO():
         return str(result.inserted_id)
 
     def modificaImpianto(impianto: ImpiantoDiIrrigazione):
-        """
-            Questo metodo modifica un impianto di irrigazione sul database
-        """
+        '''
+        Questo metodo permette la modifica di un impianto di irrigazione sul database
+
+        Parametri
+        ----------
+        impianto: ImpiantoDiIrrigazione
+            istanza su cui effetuare la modifica
+
+        Returns
+        -------
+        '''
         impianti.update_one(
             {"_id": ObjectId(impianto.id)},
             {
@@ -72,9 +142,19 @@ class ImpiantoDiIrrigazioneDAO():
         )
 
     def attivaImpianto(id: str):
-        """
-            Questo metodo attiva un impianto di irrigazione sul database
-        """
+        '''
+        Questo metodo attiva un impianto di irrigazione sul database tramite il suo id
+
+        Parametri
+        ----------
+        id: str
+            id del Impianto da attivare 
+
+        Returns
+        -------
+        Result : int
+            Restituisce un valore intero per il controllo
+        '''
         result = impianti.update_one(
             {"_id": ObjectId(id)},
             {
@@ -87,9 +167,19 @@ class ImpiantoDiIrrigazioneDAO():
         return result.modified_count == 1
 
     def disattivaImpianto(id: str):
-        """
-            Questo metodo disattiva un impianto di irrigazione sul database
-        """
+        '''
+        Questo metodo disattiva un impianto di irrigazione sul database tramite il suo id
+
+        Parametri
+        ----------
+        id: str
+            id del Impianto da disattivare 
+
+        Returns
+        -------
+        Result : int
+            Restituisce un valore intero per il controllo
+        '''
         result = impianti.update_one(
             {"_id": ObjectId(id)},
             {
@@ -102,7 +192,15 @@ class ImpiantoDiIrrigazioneDAO():
         return result.modified_count == 1
 
     def eliminaImpianto(id: str):
-        """
-            Questo metodo elimina un impianto di irrigazione sul database
-        """
+        '''
+        Questo metodo elimina un impianto di irrigazione sul database tramite il suo id
+
+        Parametri
+        ----------
+        id: str
+            id del Impianto da eliminare
+
+        Returns
+        -------
+        '''
         impianti.delete_one({"_id": ObjectId(id)})
