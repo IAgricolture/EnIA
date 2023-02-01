6338da0a7e38cf289194e02d72756f9bb193ff50
Paneenutella0654 <104584785+Paneenutella0654@users.noreply.github.com>
2023-02-01 12:48:51 +0100

    Chore: Added docstring to EventoDAO

--- src/logic/Storage/EventoDAO.py
+++ src/logic/Storage/EventoDAO.py
@@ -6,12 +6,48 @@ from bson.objectid import ObjectId
 
 
 class EventoDAO():
+    
+    '''
+    Classe DAO di Evento
+
+    ...
+
+    Attributi
+    ----------
+    None
+
+    Metodi
+    -------
+    findEvento(id: str):
+        Questo metodo trova un Evento sul database, usando il suo ObjectId
+    creaEvento(evento: Evento):
+        Questo metodo ci permette l'istanziazione di un evento sul database
+    findEventiByTerreno(idTerreno: str):       
+        Questo metodo trova gli eventi associati ad un detereminato terreno grazie da suo id passato in input 
+    cancellaTuttiEventiByTerreno(idTerreno: str):
+        Questo metodo cancella gli eventi associati ad un detereminato terreno grazie da suo id passato in input
+    cancellaEvento(idEvento: str):
+        Questo metodo cancella un evento specifico trimite il suo id
+    
+
+    '''
 
     def findEvento(id: str) -> Evento:
-        """
-            Questo metodo trova un Evento sul database, usando il suo ObjectId
-            :return: Evento
-        """
+        
+        '''
+        Questo metodo trova un Evento sul database, usando il suo ObjectId
+
+        Parametri
+        ----------
+        id : str
+            id dell Evento
+
+        Returns
+        -------
+        eventoTrovato : Evento
+            Restituisce l'evento avente come id quello passato in input
+        '''
+        
         trovato = eventi.find_one({"_id": ObjectId(id)})
         id = str(trovato.get("_id"))
         titolo = str(trovato.get("titolo"))
@@ -31,9 +67,21 @@ class EventoDAO():
         return eventoTrovato
 
     def creaEvento(evento: Evento) -> str:
-        """
-            Questo metodo instanzia un evento sul database
-        """
+        
+        '''
+        Questo metodo ci permette l'istanziazione di un evento sul database
+
+        Parametri
+        ----------
+        evento : Evento
+            evento da creare
+
+        Returns
+        -------
+        result.inserted_id : str
+            id dell evento appena creato
+        '''
+        
         result = eventi.insert_one({
             "titolo": evento.titolo,
             "descrizione": evento.descrizione,
@@ -47,6 +95,20 @@ class EventoDAO():
         return str(result.inserted_id)
 
     def findEventiByTerreno(idTerreno: str):
+        '''
+        Questo metodo trova gli eventi associati ad un detereminato terreno grazie da suo id passato in input
+
+        Parametri
+        ----------
+        idTerreno : str
+            id del Terreno
+
+        Returns
+        -------
+        eventiTrovati : list Eventi
+            Restituisce la lista degli eventi
+        '''
+        
         # trova eventi sul database con l'id del terreno
         print(idTerreno)
         eventiTrovati = eventi.find({"terreno": idTerreno})
@@ -61,7 +123,31 @@ class EventoDAO():
         return list(eventiTrovati)
 
     def cancellaTuttiEventiByTerreno(idTerreno: str):
+        '''
+        Questo metodo cancella gli eventi associati ad un detereminato terreno grazie da suo id passato in input
+
+        Parametri
+        ----------
+        idTerreno : str
+            id del Terreno
+
+        Returns
+        -------
+        '''
+        
         eventiTrovati = eventi.delete_many({"terreno": idTerreno})
 
     def cancellaEvento(idEvento: str):
+        '''
+        Questo metodo cancella un evento specifico trimite il suo id
+
+        Parametri
+        ----------
+        idEvento : str
+            id dell evento
+
+        Returns
+        -------
+        '''
+        
         eventiTrovati = eventi.delete_one({"_id": ObjectId(idEvento)})
