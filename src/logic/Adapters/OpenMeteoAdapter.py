731684f5652e2ac2f6cdc3be73c9f7b291d57ef5
marialombardi861 <marialombardi828@gmail.com>
2023-02-01 09:51:00 +0100

    Chore: Standard pep8 for adapter

--- src/logic/Adapters/OpenMeteoAdapter.py
+++ src/logic/Adapters/OpenMeteoAdapter.py
@@ -7,10 +7,11 @@ class OpenMeteoAdapter():
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
-        
+
     def get_data(self):
         url = "https://api.open-meteo.com/v1/forecast?"\
-        "latitude="+str(self.lat)+"&longitude="+str(self.lon)+ "&hourly=temperature_2m,relativehumidity_2m,precipitation"
+            "latitude=" + str(self.lat) + "&longitude=" + str(self.lon) + \
+            "&hourly=temperature_2m,relativehumidity_2m,precipitation"
         data = requests.get(url).json()
-        
+
         return data
--- src/logic/Adapters/SenseSquareAdapter.py
+++ src/logic/Adapters/SenseSquareAdapter.py
@@ -3,8 +3,9 @@ import requests
 
 
 class SenseSquareAdapter:
-    #prendi attributi opzionali start_date, end_date, start_time, end_time
-    def __init__(self, nazione, regione, provincia, comune, start_date=None, end_date=None, formato = "json"):
+    # prendi attributi opzionali start_date, end_date, start_time, end_time
+    def __init__(self, nazione, regione, provincia, comune,
+                 start_date=None, end_date=None, formato="json"):
         currentdate = str(datetime.now()).split(" ")
         date = currentdate[0]
         precisehour = currentdate[1].split(":")
@@ -18,12 +19,11 @@ class SenseSquareAdapter:
         self.start_date = start_date
         self.end_date = end_date
         self.formato = formato
-        
 
     def get_data_for_today(self):
         url = "https://square.sensesquare.eu:5001/placeView"
         body = {
-            "apikey": "3BK3D0LWE8DQ", #Codice API, NON MODIFICARE
+            "apikey": "3BK3D0LWE8DQ",  # Codice API, NON MODIFICARE
             "tempo": "giorno",
             "date": self.date,
             "time": self.time,
@@ -31,14 +31,14 @@ class SenseSquareAdapter:
             "regione": self.regione,
             "provincia": self.provincia,
             "comune": self.comune,
-            "zoom": "3", #0 per tutte le nazioni, 1 per tutte le regioni, 2 per tutte le province, 3 per tutti i comuni, 4 NON VA
-            "predictions": "true", #NON TOCCARE
-            "fonti":"[]" #NON TOCCARE
-            }
-        return requests.post(url=url, data = body).json()
-    
+            "zoom": "3",  # 0 per tutte le nazioni, 1 per tutte le regioni, 2 per tutte le province, 3 per tutti i comuni, 4 NON VA
+            "predictions": "true",  # NON TOCCARE
+            "fonti": "[]"  # NON TOCCARE
+        }
+        return requests.post(url=url, data=body).json()
+
     def get_data_time_interval(self):
-        #se gli attributi opzionali sono none lancia eccezione
+        # se gli attributi opzionali sono none lancia eccezione
         if self.start_date is None or self.end_date is None:
             raise Exception("Missing optional attributes")
         url = "https://square.sensesquare.eu:5001/download"
@@ -46,7 +46,7 @@ class SenseSquareAdapter:
             "apikey": "3BK3D0LWE8DQ",
             "req_type": "daily",
             "zoom": "3",
-            "start_date": self.start_date,    #Formato deve essere YYYY-MM-DD
+            "start_date": self.start_date,  # Formato deve essere YYYY-MM-DD
             "start_hour": "0",
             "end_date": self.end_date,
             "end_hour": "0",
@@ -55,12 +55,15 @@ class SenseSquareAdapter:
             "provincia": self.provincia,
             "comune": self.comune,
             "format": self.formato
-            }
-        datiapi = requests.post(url=url, data = body).text
-        print("formato"+self.formato)
-        if(self.formato == "json"):  ##Multipli oggetti JSON, ne faccio un parse decente in un array di oggetti json.
-            arrayDati = datiapi.split("\n") #Ne ottengo un array
-            arrayDati.pop(len(arrayDati) - 1) #Rimuovo un elemento vuoto creato con lo split, all'ultimo posto       
+        }
+        datiapi = requests.post(url=url, data=body).text
+        print("formato" + self.formato)
+        # Multipli oggetti JSON, ne faccio un parse decente in un array di
+        # oggetti json.
+        if (self.formato == "json"):
+            arrayDati = datiapi.split("\n")  # Ne ottengo un array
+            # Rimuovo un elemento vuoto creato con lo split, all'ultimo posto
+            arrayDati.pop(len(arrayDati) - 1)
             return arrayDati
         else:
-            return datiapi  #Qualsiasi altro formato, è buono così com'è.
+            return datiapi  # Qualsiasi altro formato, è buono così com'è.
