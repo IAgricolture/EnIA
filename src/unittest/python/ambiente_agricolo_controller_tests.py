5495cb5f6212cbedb139dc81d335bd6cca1c0e1c
benedettoscala <scala.benedetto@outlook.it>
2023-02-05 20:15:23 +0100

    test for dettagli in AmbienteAgricoloController

--- src/unittest/python/ambiente_agricolo_controller_tests.py
+++ src/unittest/python/ambiente_agricolo_controller_tests.py
@@ -7,9 +7,15 @@ import requests
 
 
 
+
+
+
+
 sys.path.append(os.path.abspath(os.path.join('.' )))
 from unittest.mock import patch
 from src import app
+from src.logic.Storage.TerrenoDAO import TerrenoDAO
+from src.logic.AmbienteAgricolo.AmbienteAgricoloService import AmbienteAgricoloService
 
 
 class AmbienteAgricoloControllerTests(unittest.TestCase):
@@ -47,6 +53,135 @@ class AmbienteAgricoloControllerTests(unittest.TestCase):
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.json, {'TerrenoAggiunto': True})
 
-                         
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.trovaTerreno')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercaPosizione')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercaInquinamento')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercalat')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercalon')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercaMeteo')
+    def test_dettagli(self, mock_cercaMeteo, mock_cercalon, mock_cercalat, mock_cercaInquinamento, mock_cercaPosizione, mock_trovaTerreno):
+        with self.app:
+            self.app.post("/login", data={"email": "prova@gmail.com", "password": "password", "next": "visualizzaTerreni"})
+            # Definire i dati di test per i mock
+            terreno = {'id': 1, 'nome': 'Terreno 1', 'coltura': 'Pomodori', 'stadio_crescita': 'Semina', 'posizione': 'Latitudine: 40.730610, Longitudine: -73.935242', 'preferito': False, 'priorita': 1, 'proprietario': 1}
+            posizione = {'lat': '40.730610', 'lon': '-73.935242', 'address': {'town': 'New York', 'county': 'New York County', 'state': 'New York', 'country': 'United States of America'}}
+            inquinamento = {'value': 10.0, 'unit': 'µg/m³'}
+            lat = '40.730610'
+            lon = '-73.935242'
+            meteo = {'temperature': '20.0', 'humidity': '50.0', 'wind_speed': '5.0', 'wind_direction': 'N'}
+            
+            # Configurare i mock per restituire i dati di test
+            mock_trovaTerreno.return_value = terreno
+            mock_cercaPosizione.return_value = posizione
+            mock_cercaInquinamento.return_value = inquinamento
+            mock_cercalat.return_value = lat
+            mock_cercalon.return_value = lon
+            mock_cercaMeteo.return_value = meteo
+            print(AmbienteAgricoloService.trovaTerreno(1))
+            # Invocare la funzione dettagli con una richiesta GET
+            response = self.app.get('/dettagliterreno?idTerreno=1')
+
+            # Verificare che la risposta sia corretta
+            #verificare che la risposta sia corretta
+            self.assertEqual(response.status_code, 200)
+            
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.trovaTerreno')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercaPosizione')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercaInquinamento')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercalat')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercalon')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercaMeteo')
+    def test_dettagli_key_error(self, mock_cercaMeteo, mock_cercalon, mock_cercalat, mock_cercaInquinamento, mock_cercaPosizione, mock_trovaTerreno):
+        with self.app:
+            self.app.post("/login", data={"email": "prova@gmail.com", "password": "password", "next": "visualizzaTerreni"})
+            # Definire i dati di test per i mock
+            terreno = {'id': 1, 'nome': 'Terreno 1', 'coltura': 'Pomodori', 'stadio_crescita': 'Semina', 'posizione': 'Latitudine: 40.730610, Longitudine: -73.935242', 'preferito': False, 'priorita': 1, 'proprietario': 1}
+            posizione = {'lat': '40.730610', 'lon': '-73.935242', 'address': {'town': 'New York', 'county': 'New York County', 'state': 'New York', 'country': 'United States of America'}}
+            inquinamento = {'value': 10.0, 'unit': 'µg/m³'}
+            lat = '40.730610'
+            lon = '-73.935242'
+            meteo = {'temperature': '20.0', 'humidity': '50.0', 'wind_speed': '5.0', 'wind_direction': 'N'}
+            
+            # Configurare i mock per restituire i dati di test
+            mock_trovaTerreno.return_value = terreno
+            mock_cercaPosizione.return_value = posizione
+            mock_cercaInquinamento.return_value = inquinamento
+            mock_cercalat.return_value = lat
+            mock_cercalon.return_value = lon
+            mock_cercaMeteo.return_value = meteo
+            print(AmbienteAgricoloService.trovaTerreno(1))
+            # Invocare la funzione dettagli con una richiesta GET
+            response = self.app.get('/dettagliterreno?idTerreno=1')
+
+            # Verificare che la risposta sia corretta
+            #verificare che la risposta sia corretta
+            self.assertEqual(response.status_code, 200)
+            #controlla che sia stata lanciata l'eccezione KeyError
+            self.assertRaises(KeyError)
+            
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.trovaTerreno')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercaPosizione')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercaInquinamento')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercalat')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercalon')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercaMeteo')
+    def test_dettagli_city_in_address(self, mock_cercaMeteo, mock_cercalon, mock_cercalat, mock_cercaInquinamento, mock_cercaPosizione, mock_trovaTerreno):
+        with self.app:
+            self.app.post("/login", data={"email": "prova@gmail.com", "password": "password", "next": "visualizzaTerreni"})
+            # Definire i dati di test per i mock
+            terreno = {'id': 1, 'nome': 'Terreno 1', 'coltura': 'Pomodori', 'stadio_crescita': 'Semina', 'posizione': 'Latitudine: 40.730610, Longitudine: -73.935242', 'preferito': False, 'priorita': 1, 'proprietario': 1}
+            posizione = {'lat': '40.730610', 'lon': '-73.935242', 'address': {'city': 'New York', 'county': 'New York County', 'state': 'New York', 'country': 'United States of America'}}
+            inquinamento = {'value': 10.0, 'unit': 'µg/m³'}
+            lat = '40.730610'
+            lon = '-73.935242'
+            meteo = {'temperature': '20.0', 'humidity': '50.0', 'wind_speed': '5.0', 'wind_direction': 'N'}
+            
+            # Configurare i mock per restituire i dati di test
+            mock_trovaTerreno.return_value = terreno
+            mock_cercaPosizione.return_value = posizione
+            mock_cercaInquinamento.return_value = inquinamento
+            mock_cercalat.return_value = lat
+            mock_cercalon.return_value = lon
+            mock_cercaMeteo.return_value = meteo
+            print(AmbienteAgricoloService.trovaTerreno(1))
+            # Invocare la funzione dettagli con una richiesta GET
+            response = self.app.get('/dettagliterreno?idTerreno=1')
+
+            # Verificare che la risposta sia corretta
+            #verificare che la risposta sia corretta
+            self.assertEqual(response.status_code, 200)
+            
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.trovaTerreno')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercaPosizione')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercaInquinamento')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercalat')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercalon')
+    @patch('src.logic.AmbienteAgricolo.AmbienteAgricoloService.AmbienteAgricoloService.cercaMeteo')
+    def test_dettagli_village_in_address(self, mock_cercaMeteo, mock_cercalon, mock_cercalat, mock_cercaInquinamento, mock_cercaPosizione, mock_trovaTerreno):
+        with self.app:
+            self.app.post("/login", data={"email": "prova@gmail.com", "password": "password", "next": "visualizzaTerreni"})
+            # Definire i dati di test per i mock
+            terreno = {'id': 1, 'nome': 'Terreno 1', 'coltura': 'Pomodori', 'stadio_crescita': 'Semina', 'posizione': 'Latitudine: 40.730610, Longitudine: -73.935242', 'preferito': False, 'priorita': 1, 'proprietario': 1}
+            posizione = {'lat': '40.730610', 'lon': '-73.935242', 'address': {'village': 'New York', 'county': 'New York County', 'state': 'New York', 'country': 'United States of America'}}
+            inquinamento = {'value': 10.0, 'unit': 'µg/m³'}
+            lat = '40.730610'
+            lon = '-73.935242'
+            meteo = {'temperature': '20.0', 'humidity': '50.0', 'wind_speed': '5.0', 'wind_direction': 'N'}
+            
+            # Configurare i mock per restituire i dati di test
+            mock_trovaTerreno.return_value = terreno
+            mock_cercaPosizione.return_value = posizione
+            mock_cercaInquinamento.return_value = inquinamento
+            mock_cercalat.return_value = lat
+            mock_cercalon.return_value = lon
+            mock_cercaMeteo.return_value = meteo
+            print(AmbienteAgricoloService.trovaTerreno(1))
+            # Invocare la funzione dettagli con una richiesta GET
+            response = self.app.get('/dettagliterreno?idTerreno=1')
+
+            # Verificare che la risposta sia corretta
+            #verificare che la risposta sia corretta
+            self.assertEqual(response.status_code, 200)
+            
 if  __name__ == '__main__':
     unittest.main()
